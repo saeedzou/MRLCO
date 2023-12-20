5feff1a5905231d6ec16d4bd9e92f56fa263481d
Saeedreza Zouashkiani <saeedzou2012@gmail.com>
2023-12-20 12:46:19 +0330

    Add process_csvs.py, test.ipynb, and group_evaluator.py

--- group_evaluator.py
+++ group_evaluator.py
@@ -12,6 +12,8 @@ c = Config(data)
 
 for i in range(1, 11):
     c.load_path = f"exp_Att/h_256_inner-step_1/meta_model_{i * 100}_inner_1.ckpt"
+    c.adaptation_steps = 1
+    c.iter = i * 100
     # dump config
     with open('new.json', 'w') as f:
         json.dump(c.__dict__, f, indent=2)
